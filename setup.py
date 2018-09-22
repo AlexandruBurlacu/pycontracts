54ed2552ab946d652cfd7449d1878bbd287585cc
Alex Burlacu <alexburlacu96@gmail.com>
2018-09-22 16:10:10 +0300

    update .gitignore

--- setup.py
+++ setup.py
@@ -8,7 +8,6 @@ setuptools.setup(
     version="0.1.0",
     author="Alex Burlacu",
     author_email="alexburlacu96@gmail.com",
-    short_description="A contracts programming Python package",
     long_description=long_description,
     long_description_content_type="text/markdown",
     packages=setuptools.find_packages(),
