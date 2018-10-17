7bed83d1c8c44a8584f2f6a731a4e93cec263876
Alex Burlacu <alexburlacu96@gmail.com>
2018-10-17 23:20:18 +0300

    small fix in Exception clases

--- pycontracts/exceptions.py
+++ pycontracts/exceptions.py
@@ -1,18 +1,24 @@
+import sys, traceback
+
 class PreconditionViolationError(Exception):
 
     def __init__(self, args, kwargs, msg):
         self.message = msg
-        self.args = args
-        self.kwargs = kwargs
+        self.in_args = args
+        self.in_kwargs = kwargs
+
+        super(PreconditionViolationError, self).__init__(msg)
 
     def __str__(self):
-        return "{}. [Inp]: {}, {}".format(self.message, self.args, self.kwargs)
+        return "{}. [Inp]: {}, {}".format(self.message, self.in_args, self.in_kwargs)
 
 class PostconditionViolationError(Exception):
 
     def __init__(self, ret_vals, msg):
         self.message = msg
         self.ret_vals = ret_vals
+
+        super(PostconditionViolationError, self).__init__(msg)
 
     def __str__(self):
         return "{}. [Out]: {}".format(self.message, self.ret_vals)
